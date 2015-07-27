7aa2232c5e8db44c43d8e1a40d3677238f868fa7
marb <marb61a@gmail.com>
2015-07-27 08:36:11 +0000

    added login view and updated pipeline

--- assets/js/public/login/LoginCtrl.js
+++ assets/js/public/login/LoginCtrl.js
@@ -1,12 +1,11 @@
 angular.module('LoginMod').controller('LoginCtrl',['$scope', '$http','toastr', function($scope, $http, toastr){
-	console.log('Login Controller initialized...');
 
 	$scope.runLogin = function(){
 		$http.put('/login',{
 			email: $scope.email,
 			password: $scope.password
 		}).then(function onSuccess(){
-			window.location = '/dashboard';
+			window.location = '/';
 		}).catch(function onError(err){
 			if(err.status == 400 || 404){
 				toastr.error('Invalid Credentials', 'Error', {

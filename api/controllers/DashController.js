d64dcc1e1635aec536634067fc9d8824bb480a81
marb <marb61a@gmail.com>
2015-07-31 08:55:49 +0000

    adding view dependencied and debugging

--- api/controllers/DashController.js
+++ api/controllers/DashController.js
@@ -1,21 +1,24 @@
 module.exports = {
-    checkUser : function(req, res){
-        if(!req.session.me){
-            console.log('You are NOT logged in');
+	
+	checkUser: function(req, res){
+		if(!req.session.me){
+			console.log('You are NOT logged in');
 			return res.view('login');
-        }else{
-            console.log('You are now logged in');
+		} else {
+			console.log('You are logged in');
 			return res.view('dashboard');
-        }
-    },
-    
-    getUser : function(req, res){
-        User.findOne({id: req.session.me}, function(err, user){
-            if(err){
-                res.negotiate(err);
-            }
-            
-            return res.send(user);
-        })   
-    }
+		}
+	},
+
+	getUser: function(req, res){
+		console.log('Running getUser');
+
+		User.findOne({id: req.session.me}, function(err, user){
+			if(err){
+				res.negotiate(err);
+			}
+
+			return res.send(user);
+		})
+	}
 };
--- assets/js/private/dashboard/DashCtrl.js
+++ assets/js/private/dashboard/DashCtrl.js
@@ -1,5 +1,7 @@
 angular.module('DashMod').controller('DashCtrl',['$scope', '$http', function($scope, $http){
 	$scope.getUser = function(){
+		console.log('Getting User...');
+
 		$http.get('/getuser')
 		.then(function onSuccess(user){
 			$scope.user = user.data;
